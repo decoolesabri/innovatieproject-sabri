c208a9ef82c9e9f7c51a27f198d62179a44cfde3
decoolesabri <sabritafersit1@gmail.com>
2025-12-25 13:24:21 +0100

    Add BaseCard component and integrate into App
    Introduced a reusable BaseCard component with title, content, and delete button. Updated App.jsx to use BaseCard and display test content.

--- src/components/Card/BaseCard.jsx
+++ src/components/Card/BaseCard.jsx
@@ -1,5 +1,27 @@
-// titel bovenaan
-// content in het midden (children)
-// verwijderknop onderaan
+export default function BaseCard({ title, onDelete, children }) {
+    return (
+        <div className="w-72 bg-white rounded-xl shadow-md flex flex-col">
 
-// BaseCard is een presentational component die hergebruikt wordt door alle kaarttypes.
+            {/* Header van het kaartje */}
+            <div className="px-4 py-2 border-b font-semibold">
+                {title}
+            </div>
+
+            {/* Content van het kaartje */}
+            <div className="flex-1 p-4">
+                {children}
+            </div>
+
+            {/* Footer van het kaartje */}
+            <div className="px-4 py-2 border-t">
+                <button
+                onClick={onDelete}
+                className="w-full text-sm text-red-600 hover:text-red-800"
+                >
+                    Verwijderen
+                </button>
+            </div>
+
+        </div>
+    )
+}
